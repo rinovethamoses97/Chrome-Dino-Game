2a86a885fb4cfab193a4449a2e120c644f7ab13b
rino <rinovethamoses97@gmail.com>
2019-06-13 21:20:15 +0530

    Dino jumping done

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
+var dino;
 function setup(){
-
+    createCanvas(800,300);
+    background(0);
+    dino=new Dino();
 }
 function draw(){
-    
+    background(0);
+    dino.update();
+    dino.show();
+}
+function keyPressed(){
+    if(keyCode==32){
+        if(!dino.inAir)
+            dino.jump();
+    }
 }
